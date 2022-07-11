5c196147944c0321353ae3e5d4e22729d3f27295
Rafael Olivares <akolor@Federicas-Mac-mini.local>
2022-07-11 12:59:04 -0400

    error en texto

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -2,7 +2,7 @@ const siteMetadata = {
   title: 'Forrest Media',
   author: 'Forrest Media Group',
   headerTitle: 'Forrest Media',
-  description: 'Riesgo financiero, ciberseguridad y la crimen organizado.',
+  description: 'Riesgo financiero, ciberseguridad y crimen organizado.',
   language: 'en-us',
   theme: 'system', // system, dark or light
   siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
