0cadd5965951475283aeda0a9c88553f6e03f7ce
Rafael Olivares <rafael.olivares@bairesdev.com>
2023-12-13 19:20:40 -0400

    change to light mode

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -4,7 +4,7 @@ const siteMetadata = {
   headerTitle: 'PanamEconomics',
   description: ['Gobierno Corporativo', 'Negocios', 'Riego Corporativo'],
   language: 'en-us',
-  theme: 'system', // system, dark or light
+  theme: 'light', // system, dark or light
   siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
   siteRepo: 'https://github.com/timlrx/tailwind-nextjs-starter-blog',
   siteLogo: '/static/images/logo.png',
